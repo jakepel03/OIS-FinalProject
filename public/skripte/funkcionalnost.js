bedfc466425495dbfd24bc422f3ee534904d62eb
jakepel03 <jaka.pelko03@gmail.com>
2023-05-21 11:53:11 +0200

    Dopolnitev funkcionalnosti in implementacija vseckanja

--- public/skripte/funkcionalnost.js
+++ public/skripte/funkcionalnost.js
@@ -1,6 +1,6 @@
 var baza = "jakepel03";
 var baseUrl = "https://teaching.lavbic.net/api/OIS/baza/" + baza + "/podatki/";
-
+var SENSEI_RACUN = "0x545FC391774843679197086d660fe90BAD6c4a57";
 
 narediSeznamDestinacij();
 // naredi poizvedbo o vremenu na izbran API
@@ -15,7 +15,7 @@ function narediPoizvedbo() {
         );
     } else {
         $.ajax({
-            url: "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + imeDestinacije + "/next7days?unitGroup=metric&elements=datetime%2CdatetimeEpoch%2Cname%2Clatitude%2Clongitude%2Ctempmax%2Ctempmin%2Ctemp%2Cwindspeed%2Cwindspeedmax%2Cpressure%2Cuvindex%2Csunrise%2Csunset%2Cconditions%2Cdescription&include=days&key=FQJKS22Y4MZ97GQXL5C7ZBFV6&contentType=json",
+            url: "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + imeDestinacije + "/next7days?unitGroup=metric&elements=datetime%2Ctemp%2Cwindspeedmax%2Cconditions&include=days&key=FQJKS22Y4MZ97GQXL5C7ZBFV6&contentType=json",
             type: "GET",
             success: function (podatki) {
                 dodajVBazo(imeDestinacije, podatki);
@@ -45,7 +45,7 @@ function dodajVBazo(imeDestinacije, vreme) {
         success: function (data) {
             $("#kreirajSporocilo").html(
                 "<div class='alert alert-success alert-dismissible fade show mt-3 mb-0'>" +
-                "Destinacija je bila uspešno dodana v bazo <b>" +
+                "Destinacija je bila uspešno dodana v bazo" +
                 "<button type='button' class='btn-close' data-bs-dismiss='alert'></button>" +
                 "</div>"
             );
@@ -165,6 +165,166 @@ function izrisiGraf(podatkiGraf) {
     });
     chart.render();
 }
+
+/**
+ * Funkcija za všečkanje z 0,1 - 1 Ethereum kriptovalute
+ */
+const vseckaj = async (modalnoOknoVseckaj) => {
+    try {
+        var prejemnikDenarnica = $("#eth-racun").attr("denarnica");
+        var posiljateljDenarnica = $("#denarnica-posiljatelja").text();
+        var kolicinaETH = $("#kolicinaETH").val();
+
+        let rezultat = await web3.eth.sendTransaction({
+            from: posiljateljDenarnica,
+            to: prejemnikDenarnica,
+            value: kolicinaETH * Math.pow(10, 18),
+        });
+
+        // ob uspešni transakciji
+        if (rezultat) {
+            modalnoOknoVseckaj.hide();
+        } else {
+            // neuspešna transakcija
+            $("#napakaVseckanje").html(
+                "<div class='alert alert-danger' role='alert'>" +
+                "<i class='fas fa-exclamation-triangle me-2'></i>" +
+                "Prišlo je do napake pri transakciji!" +
+                "</div>"
+            );
+        }
+    } catch (e) {
+        // napaka pri transakciji
+        $("#napakaVseckanje").html(
+            "<div class='alert alert-danger' role='alert'>" +
+            "<i class='fas fa-exclamation-triangle me-2'></i>" +
+            "Prišlo je do napake pri transakciji: " + e +
+            "</div>"
+        );
+    }
+};
+
+
+function okrajsajNaslov(vrednost) {
+    return vrednost.substring(0, 4) + "..." + vrednost.substring(vrednost.length - 3, vrednost.length);
+}
+
+/**
+ * Funkcija za prijavo Ethereum denarnice v testno omrežje
+ */
+const prijavaEthereumDenarnice = async (modalnoOknoPrijava) => {
+    try {
+        let rezultat = await web3.eth.personal.unlockAccount(
+            $("#denarnica").val(),
+            $("#geslo").val(),
+            300);
+
+        // ob uspešni prijavi računa
+        if (rezultat) {
+            prijavljenRacun = $("#denarnica").val();
+            $("#eth-racun").html(okrajsajNaslov($("#denarnica").val()) + " (5 min)");
+            $("#eth-racun").attr("denarnica", $("#denarnica").val());
+            $("#gumb-vseckaj-start").removeAttr("disabled");
+            modalnoOknoPrijava.hide();
+        } else {
+            // neuspešna prijava računa
+            $("#napakaPrijava").html(
+                "<div class='alert alert-danger' role='alert'>" +
+                "<i class='fas fa-exclamation-triangle me-2'></i>" +
+                "Prišlo je do napake pri odklepanju!" +
+                "</div>"
+            );
+        }
+    } catch (napaka) {
+        // napaka pri prijavi računa
+        $("#napakaPrijava").html(
+            "<div class='alert alert-danger' role='alert'>" +
+            "<i class='fas fa-exclamation-triangle me-2'></i>" +
+            "Prišlo je do napake pri odklepanju: " + napaka +
+            "</div>"
+        );
+
+    }
+};
+/**
+ * Funkcija za prikaz všečkov v tabeli
+ */
+const dopolniTabeloVseckov = async () => {
+    try {
+        let steviloBlokov = (await web3.eth.getBlock("latest")).number;
+        let st = 1;
+        $("#seznam-vseckov").html("");
+        for (let i = 0; i <= steviloBlokov; i++) {
+            let blok = await web3.eth.getBlock(i);
+
+            for (let txHash of blok.transactions) {
+                let tx = await web3.eth.getTransaction(txHash);
+                if (SENSEI_RACUN == tx.to) {
+                    $("#seznam-vseckov").append("\
+                    <tr>\
+                        <th scope='row'>" + st++ + "</th>\
+                        <td>" + okrajsajNaslov(tx.hash) + "</td>\
+                        <td>" + okrajsajNaslov(tx.from) + "</td>\
+                        <td>" + parseFloat(web3.utils.fromWei(tx.value)) + " <i class='fa-brands fa-ethereum'></i></td>\
+                    </tr>");
+                }
+            }
+        }
+    } catch (e) {
+        alert(e);
+    }
+};
+
+
+/**
+ * Funkcija za dodajanje poslušalcev modalnih oken
+ */
+function poslusalciModalnaOkna() {
+    const modalnoOknoPrijava = new bootstrap.Modal(document.getElementById('modalno-okno-prijava'), {
+        backdrop: 'static'
+    });
+
+    const modalnoOknoVseckaj = new bootstrap.Modal(document.getElementById('modalno-okno-vseckanje'), {
+        backdrop: 'static'
+    });
+
+    $("#denarnica,#geslo").keyup(function (e) {
+        if ($("#denarnica").val().length > 0 && $("#geslo").val().length > 0)
+            $("#gumb-potrdi-prijavo").removeAttr("disabled");
+        else
+            $("#gumb-potrdi-prijavo").attr("disabled", "disabled");
+    });
+
+    $("#gumb-potrdi-prijavo").click(function (e) {
+        prijavaEthereumDenarnice(modalnoOknoPrijava);
+    });
+
+    $("#potrdi-vsecek").click(function (e) {
+        vseckaj(modalnoOknoVseckaj);
+    });
+
+    var modalnoOknoVsecki = document.getElementById('modalno-okno-vseckanje');
+    modalnoOknoVsecki.addEventListener('show.bs.modal', function (event) {
+        var prijavljenaDenarnica = $("#eth-racun").attr("denarnica");
+        $("#prejemnik").text(SENSEI_RACUN);
+        $("#denarnica-posiljatelja").text(prijavljenaDenarnica);
+    });
+
+    var modalnoOknoSeznamVseckov = document.getElementById('modalno-okno-seznam-vseckov');
+    modalnoOknoSeznamVseckov.addEventListener('show.bs.modal', function (event) {
+        dopolniTabeloVseckov();
+    });
+}
+
+$(document).ready(function () {
+
+    /* Povežemo se na testno Ethereum verigo blokov */
+    web3 = new Web3("https://sensei.lavbic.net:8546");
+
+    /* Dodamo poslušalce */
+    poslusalciModalnaOkna();
+
+});
 
 
 
